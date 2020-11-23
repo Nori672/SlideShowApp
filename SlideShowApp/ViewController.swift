45b7c8fe18a6c428f98fb25647e9b068403fbced
Norihiro <kanjam8session@gmail.com>
2020-11-23 22:28:35 +0900

    Initial Commit

---
+++ SlideShowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideShowApp
+//
+//  Created by Norihiro.Nakano on 2020/11/23.
+//  Copyright © 2020 Norihiro.Nakano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
